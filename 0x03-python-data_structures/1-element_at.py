f8db0ae8946ca6071ee008f6109df296bd9bb396
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-06-14 16:09:57 -0700

    fix error

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-def element_at(my_list, idx):
 
+def element_at(my_list, idx):
     l = len(my_list) - 1
+
     if (idx <= l and idx >= 0):
         return (my_list[idx])
+
     else:
         return None
