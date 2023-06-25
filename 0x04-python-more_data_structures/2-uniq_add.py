74af39752edba1786f18021c5f62795c663a43d4
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-06-25 13:56:12 -0700

    fix

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
+
 def uniq_add(my_list=[]):
-   x = []
-   sum = 0
-   for i in my_list:
-      if i not in x:
-         sum += i
-         x.append(i)
-   return sum
+    x = []
+    sum = 0
+    for i in my_list:
+        if i not in x:
+            sum += i
+            x.append(i)
+    return sum
