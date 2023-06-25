888815b139ebb2c9ed1f0e9fdf1d3f7b647d1f9a
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-06-25 12:35:40 -0700

    0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,4 @@
 def square_matrix_simple(matrix=[]):
     for r in range(len(matrix):
             for c in range(len(matrix[0])):
-                matrix[r][c] *= matrix[r][c]
+                new[r][c] = matrix[r][c]**2
