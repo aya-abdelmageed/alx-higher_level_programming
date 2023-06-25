4703bb2c73f13cfd09a8add91312121932eec65c
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-06-25 12:54:47 -0700

    fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    for r in range(len(matrix):
-            for c in range(len(matrix[0])):
-                new[r][c] = matrix[r][c]**2
+    new = [[x ** 2 for x in m] for m in matrix]]
     return new
