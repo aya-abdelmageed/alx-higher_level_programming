475170135a5f96b54dda435b9b8a811e99df150a
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-06-26 14:19:23 -0700

    fix 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,5 +16,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             ans = 0
             print("wrong type")
+        finally:
+            pass
         l.append(ans)
     return (l)
