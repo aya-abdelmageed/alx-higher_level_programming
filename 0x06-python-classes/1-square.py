cdc4f453426f218fc6c6bcf31bab27b27bbdf20e
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-06-29 12:03:06 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size):
+        """initialize square args"""
+        self.__size = size
