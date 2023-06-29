fb8e4eb50bf8ff970517cb55fea006868e16141d
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-06-29 12:33:27 -0700

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class Square that defines a squar"""
+
+
+class Square:
+    """class Square that defines a squar"""
+    def __init__(self, size = 0):
+        """initialize the data"""
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        is size < 0:
+            raise ValueError("size must be >= 0")
