296701fdfcd2043850a2c2a5fe52972c4a8e014b
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-07-18 13:39:40 -0700

    add tasks

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+class MyList(list):
+    """class MyList that inherits from list"""
+    pass
+
+    def print_sorted(self):
+        """ prints the list, but sorted (ascending sort)"""
+        print(sorted(list(self)))
