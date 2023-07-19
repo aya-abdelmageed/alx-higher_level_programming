2dd946fd902d990c97c24150f5c2b8c7457474d4
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-07-19 04:04:14 -0700

    fix 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,9 +6,11 @@ class student:
     """ class of student"""
 
     def __init__(self, first_name, last_name, age):
+        """method """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """return dir description"""
         return self.__dict__
