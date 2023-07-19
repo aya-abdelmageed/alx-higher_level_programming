1ec543d2d4cd49e104f473a87ec49e96c53a230b
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-07-19 03:53:43 -0700

    8-9

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class to JSON"""
+import json as j
+
+
+def class_to_json(obj):
+    """ dictionary description with simple data structure (list, dict ..."""
+    x = {}
+    if hasattr(obj, "__dict__"):
+        x = obj.__dict__.copy()
+    return x
