382a146e9da6cf18042e78a0b92f7efcc4d13311
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-09-11 14:53:37 -0700

    updates

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-//  prints My number: <first argument converted in integer>
-//  in the given format
+// print two arguments passed to it, in the following format: “ is ”
 
-if (isNaN(process.arg[2])) {
+if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + parseInt(process.argv[2]));
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-// prints 3 lines by using an array of string and a loop
+// prints 3 lines but by
+// using an array of string and a loop
 
-const x = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < 3; i++) {
-  console.log(x[i]);
+  console.log(lang[i]);
 }
