5cea866d20427f280eb76067388990e6ddbd77e9
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-09-12 13:43:31 -0700

    fix 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,5 +2,5 @@
 // func that returns the number of occurrences in a list
 
 exports.nbOccurences = function (list, searchElement) {
-  return list.reduce((c, i) => i === searchElement ? c++ : c, 0);
+  return list.reduce((c, i) => i === searchElement ? c + 1 : c, 0);
 };
