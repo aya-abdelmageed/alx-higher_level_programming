e2f76fcf115ea2a1c7391ab97a1ac388b08defae
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-09-12 13:13:45 -0700

    add square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// define class square inherits from Square of 5-square.js
+
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) { this.print(); } else {
+      for (let i = 0; i < this.height; i++) { console.log(c.repeat(this.width)); }
+    }
+  }
+};
