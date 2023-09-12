cfe21929ddad5589dec7223c93c8b9d7100371fe
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-09-12 08:51:46 -0700

    fix 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // prints 3 lines by using an array of string and a loop
 
-const x = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const x = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < 3; i++) {
   console.log(x[i]);
