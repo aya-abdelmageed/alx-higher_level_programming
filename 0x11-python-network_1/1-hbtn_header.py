565b60bd21786b2b94cd8644eb4d128621e23d37
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-09-29 08:33:06 -0700

    task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" sends a request to the URL and displays the value of the X-Request-Id """
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        value = html.get('X-Request-Id')
+        print(value)
